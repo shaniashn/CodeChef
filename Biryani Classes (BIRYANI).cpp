1a3d95abaef540f26321862f93d7c02ea7c3e58a
Sha Nia Siahaan <74463007+shaniashn@users.noreply.github.com>
2022-06-26 19:27:46 +0700

    Add files via upload

---
+++ Biryani Classes (BIRYANI).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, x, y, result;
+	
+	cin >> t;
+	while(t--){
+	    cin >> x >> y;
+	    result = x * y;
+	    cout << result << endl;
+	}
+	return 0;
+}
