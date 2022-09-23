a8ee0f695bc0ae14b52d383801b334a75959bd58
Sha Nia Siahaan <74463007+shaniashn@users.noreply.github.com>
2022-09-23 12:04:28 +0700

    Create Alice and Marks (MARKSTW).cpp

---
+++ Alice and Marks (MARKSTW).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, z;
+	
+	cin >> x >> y;
+	if(x >= (2*y)){
+	    cout << "yes";
+	}
+	else{
+	    cout << "no";
+	}
+	
+	return 0;
+}
