a8f8ef9614ac5cd76a0a9cfad63bfa78092e21ad
Sha Nia Siahaan <74463007+shaniashn@users.noreply.github.com>
2023-09-06 21:39:20 +0700

    CreateFitness(BasicMathPractice).cpp

---
+++ Fitness (BasicMathPractice).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, x, result;
+	
+	cin >> t;
+	
+	while (t--) {
+	    cin >> x;
+	    result = 2 * 5 * x;
+	    cout << result << endl;
+	}
+	
+	return 0;
+}
